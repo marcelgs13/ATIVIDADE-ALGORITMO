0351880dfca3868b89e29bfcfb19c2302eaaf830
marcelgs13 <119629090+marcelgs13@users.noreply.github.com>
2024-09-05 17:38:54 +0000

    commit_1

--- src/main/tree/BinarySearchTree.java
+++ src/main/tree/BinarySearchTree.java
@@ -1,12 +1,38 @@
 package tree;
-
+import java.util.ArrayList;
+import java.util.List;
 import estrut.Tree;
 
 public class BinarySearchTree implements Tree{
+    private Node root;
+
+    private static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
 
     @Override
     public boolean buscaElemento(int valor) {
-        return true;
+        return buscaElementoRecursivamente(root, valor);
+    }
+    private boolean buscaElementoRecursivamente(Node root, int valor) {
+        if (root == null)
+            return false;
+
+        if (valor == root.value)
+            return true;
+
+        if (valor < root.value)
+            return buscaElementoRecursivamente(root.left, valor);
+        else
+            return buscaElementoRecursivamente(root.right, valor);
     }
 
     @Override
